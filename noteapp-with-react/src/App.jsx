95cb94c5c8bcf5bf967f4064ab726c1e094a8fdb
MIKA ARAI <mika8500@yk.commufa.jp>
2024-01-19 00:10:52 +0900

    ノートアプリ実装

--- noteapp-with-react/src/App.jsx
+++ noteapp-with-react/src/App.jsx
@@ -1,14 +1,67 @@
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
+import { useEffect, useState } from 'react';
+import uuid from 'react-uuid';
 
 function App() {
+
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    // ローカルストレージに保存
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+    setActiveNote(notes[0].id);
+  }, []);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note)=> note.id !== id);
+    setNotes(filterNotes);
+  };
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す
+    const updatedNotesArray = notes.map((note => {
+      if(note.id === updatedNote.id) {
+        return updatedNote;
+      } else {
+        return note;
+      }
+    }));
+
+    setNotes(updatedNotesArray);
+  };
 
   return (
     <>
     <div className='App'>
-      <Sidebar />
-      <Main />
+      <Sidebar 
+        onAddNote={onAddNote} 
+        notes={notes} 
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote} 
+        setActiveNote={setActiveNote}
+      />
+      <Main activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
     </div>
     </>
   )
